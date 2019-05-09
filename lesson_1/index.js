6254afb114deed9a1a40fcb2a5508e6254cd6204
igorkulichok@gmail.com <igorkulichok@gmail.com>
2019-05-09 20:53:03 +0300

    hom

---
+++ lesson_1/index.js
@@ -0,0 +1,24 @@
+function makeGreetings(age){
+	var arr = ['год', 'года', 'лет'];
+	var value = age % 10;
+	var res;
+	if (value < 1 || value >= 5 ){result = arr[2]}
+	else if(value == 1){result = arr[0]}
+	else if(value >= 2 && value <= 4){result = arr[1]};
+	return 'Мой возраст ' + age + result;
+};
+
+
+function splitArray( arr1, arg ){
+	var newArr = new Array (Math.ceil(arr1.length / arg));
+	var smallArr = new Array(arr1.length);
+	for(var i = 0, j = 0; i < arr1.length / arg; i+= 1){
+		smallArr =  arr1.slice(j, j + arg);
+		newArr [i] = smallArr;
+	};
+	return newArr
+};
+
+
+
+
