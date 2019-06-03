8bfd5510b6afb5795450383c75903fac4cc7f33f
kulichokigor <igorkulichok@gmail.com>
2019-06-03 22:12:25 +0300

    hom

---
+++ lesson_6/index.js
@@ -0,0 +1,30 @@
+var randomEmail = require('random-email');
+var validator = require("email-validator");
+
+console.log(randomEmail())
+
+
+const generate = document.querySelector('.generate');
+const mailBox = document.querySelector('.mail-block');
+const close = document.querySelector('.close');
+const email = document.querySelector('#viev-mail');
+
+generate.addEventListener('click', function(){
+    mailBox.style.display = 'block';
+    email.value = randomEmail();
+});
+
+close.addEventListener('click', function(){
+    mailBox.style.display = 'none';
+});
+
+const validMail = document.querySelector('#validate-mail');
+validMail.addEventListener('change', function(){
+	if(validator.validate(email.value)){
+		validMail.style.border = "2px solid green"
+	}else{
+		validMail.style.border = "2px solid red"
+	}
+});
+
+
